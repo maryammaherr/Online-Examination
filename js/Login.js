4f1bf52beaa339edc9e9d83d7140601935a36a62
MohammedAbusarie <new.is.new55@gmail.com>
2022-03-04 16:17:43 +0200

    added toaster library and connected login page to the server

--- js/Login.js
+++ js/Login.js
@@ -18,3 +18,41 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+
+
+function authenticate(){
+	let username=document.getElementById("username").value;
+	let password=document.getElementById("password").value;
+	if (username == "") 
+		toastWarning("Username is empty!");
+	else if (password == "") 
+		toastWarning("Password is empty!");
+	   else {
+		$.ajax({
+		  url: "https://localhost:44311/api/Authenticate/Login",
+		  type: 'POST',
+		  data: JSON.stringify({
+			"username": username,
+            "password": password
+		  }),
+		  dataType: 'json',
+		  "headers": {
+			'Content-Type':'application/json',
+			"Access-Control-Allow-Origin":"*"
+		  },
+		  success: function (data) {
+			  console.log(data.token);
+			  localStorage.setItem('token', data.token);
+			  window.location.replace("../html/main.html");
+		  },
+		  error: function(xhr,status,error){
+			console.log(error);
+			console.log(status);
+			console.log(xhr.responseText);
+			toastError("Wrong Credentials");
+		  }
+		});
+	  }
+}
+
---
+++ js/toaster.js
@@ -0,0 +1,40 @@
+function toastWarning(msg){
+    toastr.warning(msg);
+}
+
+function toastSuccess(title,msg){
+    toastr.success(msg);
+}
+
+function toastInfo(msg){
+    toastr.info(msg);
+}
+
+function toastError(msg){
+    toastr.error(msg);
+}
+
+$(document).ready(function() {
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "3500",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+    }
+
+});
+
+
+
