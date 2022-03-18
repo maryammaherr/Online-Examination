2e4860f7b28f914786bd431b8ea57c49afc64dd7
maryammaherr <100450540+maryammaherr@users.noreply.github.com>
2022-03-18 02:29:27 +0200

    style

---
+++ js/Courses_Status.js
@@ -0,0 +1,32 @@
+var json = [
+    {
+      Course_Name: "IE",
+      Date:"22/11/2022",
+      Time: "9:00 AM",
+      Status: "10:00 AM",
+    },
+    {
+        Course_Name: "MM",
+        Date:"23/11/2022",
+        Time: "8:00 AM",
+        Status: "9:00 AM",
+      },
+      {
+        Course_Name: "DMM",
+        Date:"24/11/2022",
+        Time: "10:30 AM",
+        Status: "11:00 AM",
+      },
+      {
+        Course_Name: "DSP",
+        Date:"25/11/2022",
+        Time: "9:00 AM",
+        Status: "10:30 AM",
+      },
+      {
+        Course_Name: "AI",
+        Date:"25/11/2022",
+        Time: "11:30 AM",
+        Status: "12:30 PM",
+      },
+  ];
