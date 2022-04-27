6b7cdd5b99b3b8abc8a6704923a3fe7c9420c9f5
maryammaherr <100450540+maryammaherr@users.noreply.github.com>
2022-04-27 16:39:27 +0200

    n

---
+++ js/NotFound.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+
+    document.getElementById('logout').addEventListener("click",Log_Out);
+
+
+    function Log_Out(){
+        window.location.href=LINKS.LOGIN_PAGE;
+      }  
+
+    });
