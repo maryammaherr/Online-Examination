824bf15940bfcd26798de58f859db88dc002394a
maryammaherr <100450540+maryammaherr@users.noreply.github.com>
2022-05-20 02:13:43 +0200

    .

--- js/AdminPage.js
+++ js/AdminPage.js
@@ -3,6 +3,8 @@ window.addEventListener('load', function(){
     
     document.getElementById('Users').addEventListener("click",Manage_Users);
     document.getElementById('Schedule').addEventListener("click",Manage_Schedule);
+    document.getElementById('log_out').addEventListener("click",Log_Out)
+
 
    
   
